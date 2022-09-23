655161e9256e9750984e4500f091bffdd7680306
Christian Bergane <107763472+CBergane@users.noreply.github.com>
2022-09-23 14:01:50 +0200

    added so the board gets printed and you can make a guess shot, added so the borde gets printed back to you with the shot marked

--- run.py
+++ run.py
@@ -1,30 +1,48 @@
 
 from random import randint
 
-SHOTS = 8
-BOARD_SIZE_X = 8
-BOARD_SIZE_Y = 8
-
-
 """
 Creates a board 8*8 
 """
 board = []
 
-for i in range(0, 8):
+for x in range(0, 8):
     board.append(['O']* 8)
 
 def print_board(board):
     """
     Stack on top of each other and cleans up the board
     """
-    for i in board:
-        print (' '.join(i))
+    for row in board:
+        print(' '.join(row))
+
+print_board(board)
+
+"""
+Places CPUs ship on the board
+"""
+def random_row(board):
+    return randint(0, len(board) - 1)
+
+def random_col(board):
+    return randint(0, len(board) - 1)
         
-def player_guess():
-    """Reads a valid guess from the player"""
-    
-    while True:
-        # read the guess of the rows and columns
-        guess_rwo = read_int("Guess rwo: ")
-        guess_col = read_int("Guess column: ")
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+"""
+Player makes a shoot
+"""
+guess_row = int(input('Guess row: '))
+guess_col = int(input('Guess column: '))
+
+print(ship_row)
+print(ship_col)
+
+"""Hit or no hit"""
+if guess_row == ship_row and guess_col == ship_col:
+    print('Good shoot you hit my ship')
+else:
+    print('Bad shoot you missed my ship')
+    board[guess_row][guess_col] = 'x'
+    print_board(board)
