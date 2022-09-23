d5c9597d0555bb5c6e7ffdddd5af70352a54a2c6
Christian Bergane <107763472+CBergane@users.noreply.github.com>
2022-09-23 13:25:49 +0200

    trying to add a choice from the player on the board

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
+
+from random import randint
+
+SHOTS = 8
+BOARD_SIZE_X = 8
+BOARD_SIZE_Y = 8
+
+
 """
 Creates a board 8*8 
 """
@@ -7,9 +15,16 @@ for i in range(0, 8):
     board.append(['O']* 8)
 
 def print_board(board):
+    """
+    Stack on top of each other and cleans up the board
+    """
     for i in board:
-        print(i)
+        print (' '.join(i))
         
-        
-print_board(board)
+def player_guess():
+    """Reads a valid guess from the player"""
     
+    while True:
+        # read the guess of the rows and columns
+        guess_rwo = read_int("Guess rwo: ")
+        guess_col = read_int("Guess column: ")
